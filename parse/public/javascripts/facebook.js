9212c0bae2f0fdf2475b55309fb29292e91204f4
fuhua <emailhelenshih@gmail.com>
2014-08-15 23:38:52 +0800

    resolve conflicts, integrate,
    image on click : accurate section, image, info
    
    resolve conflicts:
    	parse/cloud/views/user.ejs
    	parse/public/javascripts/facebook.js
    	parse/public/javascripts/main.js

--- parse/public/javascripts/facebook.js
+++ parse/public/javascripts/facebook.js
@@ -1,40 +1,176 @@
+
+//In order to authenticate your app, you must pass the authorization code 
+//and your app secret to the Graph API token endpoint at 
+//https://graph.facebook.com/oauth/access_token 
+
+//If you plan on using the FB.api function to make calls to their Graph API, 
+//then you need the code to get the access token. But if you 
+//only need to authenticate the user, then what you have will do that just fine.
+
 function FacebookMod(facebookId) {
 	//this.applicationId = parseApplicationId,
 	//this.jsKey = parseJavascriptKey,
 	this.facebookId = facebookId;
+	//this.accessToken;
+	//this.currentUserID;
 	
 	this.init = function() {
 		Parse.FacebookUtils.init({
 			appId: this.facebookId,
-			status: true,
 			xfbml: true,
 			version: 'v2.0'
 		});
-	
+		
+		FB.init({
+			appId      : this.facebookId,
+			xfbml      : true,
+			version    : 'v2.0'
+        });
 	},
-	
+
 	this.login = function() {
 		this.init();
-		
-		Parse.FacebookUtils.logIn("public_profile, email, user_friends", {
+		var self = this;
+		Parse.FacebookUtils.logIn("public_profile, email, user_friends, publish_actions", {
 			success: function(user) {
-				console.log(JSON.stringify(user));	
-			 	window.location.href = "/user";
-			},
+				console.log(user);
+				if (!user.existed()){ //sign up as new user
+                    self.currentUserInfo(function(status) {
+						if (status) {
+							self.getUserLoginStaus(function(loginStatus){
+								if(loginStatus){
+									window.location.href = "/user";
+								}
+								else {
+									alert("something went wrong when calling getUserLoginStaus under FacebookUtils login");
+									window.location.href = "/";
+								}
+							});
+							
+						} else {
+							alert("Login Failed");
+							window.location.href = "/";
+						}
+					});
+					
+				} else { //response is null or have error
+					self.currentUserInfo(function(status) {
+					
+						if (status) {
+							window.location.href = "/user";
+						} else {
+							alert("Login Failed");
+							window.location.href = "/";
+						}
+					});
+				}					
+
+			},	
 			error: function(user, error) {
 			  console.log(JSON.stringify(error, null, " "));
+			  alert(JSON.stringify(error));
 			  alert("User cancelled the facebook login or did not fully authorize");
 			}
 		});
 	}, 
 	
-	function logout() {
+	this.currentUserInfo = function(callback) {
+		var self = this;
+		FB.api('/me', function(userInfo){
+			FB.api('/me/picture', function(imageURL){
+				userInfo.data = imageURL.data;
+				
+				self.insertUser(Parse.User.current(), userInfo, function(status) {
+					callback(status);
+				});
+			});
+		});
+	},
+
+	this.insertUser = function(user, userInfo, callback) {
+	
+		user.save(null, {
+			success: function(user) {
+				user.set("emailaddress", userInfo.email);
+				user.set('firstName', userInfo.first_name);
+				user.set('lastName', userInfo.last_name);
+				user.set('gender', userInfo.gender);
+				user.set('linkToFb', userInfo.link);
+				user.set('fullName', userInfo.name);
+				user.set('profilePhotoUrl', userInfo.data.url);
+				user.set('points', 0);
+				
+				user.save();
+				callback(true);
+			}, 
+			error: function (user, error) {
+				callback(false);
+			}
+		});
+	},
+
+	this.getUserLoginStaus = function(callback) {
+		FB.getLoginStatus(function(response) {
+			
+			callback(response.status, response);
+			
+			//if response.status === 'connected' 
+				// the user is logged in and has authenticated your
+				// app, and response.authResponse supplies
+				// the user's ID, a valid access token, a signed
+				// request, and the time the access token 
+				// and signed request each expire
+
+			//if response.status === 'not_authorized'
+				// the user is logged in to Facebook, 
+				// but has not authenticated your app
+			// else {
+				//the user isn't logged in to Facebook.
+		
+		});
+	},
+
+	this.getUserInfo = function(callback) {
+		this.init();
+		var user = Parse.User.current();
+
+		var json = { lastName: user.get("lastName") };
+		
+		callback(json);
+	},
+	
+	this.logout = function() {
+		alert('logging you out');
 		Parse.FacebookUtils.logOut();
 	},
 
-	function checkPermissions() {
+	this.feed = function(link){
+		alert(link);
+
+		FB.ui(
+		{
+		    method: 'share',
+		    href: link,
+		},
+		function(response) {
+			if (response && !response.error_code) {
+		      alert('Posting completed.');
+		      alert("response id recieved is" + response.objectId);
+		    } else {
+		    	console.log(response.error_code);
+		      alert('Error while posting.');
+		    }
+		  }
+		);
+			alert("FB.ui is being entered!");
+	}
+	
+
+    function checkPermissions() {
 		FB.api('/me/permissions', function(response) {
 			console.log(response);
 		});
 	};
-}
+
+
+};
--- parse/public/javascripts/main.js
+++ parse/public/javascripts/main.js
@@ -1,4 +1,118 @@
-Parse.initialize("MZVlTYAzuOWwZ1JH9xMAhlVpyEG2banAtMVaCiI3", "rOvhJctAZsQkZVkPv7nsvV4XvJ2bz00E8qm7I67A");
+var appid = "p7MiFGIkEjP4bP6L6KGhWmLzU7wKHzRdCx7fTyAe";
+var jsKey = "IhkupUEOukPUx8pHGAk5qSa8rGAWiW9ZiaxfC1IU";
+var restAPIKey = "JoEBg1fjF2AnEzrReZOWw0Du6FU9H4BdiNTs2lMS";
+var facebookMod = new FacebookMod("661965090552510");
+
+$(function() {
+  // Facebook Function Initiation
+  Parse.initialize(appid, jsKey);
+
+  // Make all of special links magically post the form
+  // when it has a particular data-action associated
+  $("a[data-action='post']").click(function(e) {
+    var el = $(e.target);
+    el.closest("form").submit();
+    return false;
+  });
+
+  $('.like').click(function(e) {
+    var metadataId = e.currentTarget.getAttribute('metadataid');
+
+    $.ajax({
+      type: "POST",
+      url: "https://api.parse.com/1/functions/likeImage",
+      headers: {
+        "X-Parse-Application-Id": appid,
+        "X-Parse-REST-API-Key": restAPIKey
+      },
+      data: {
+        metadataId: metadataId
+      }
+    }).done(function(msg) {
+      console.log("Data Saved");
+    });
+  });
+
+  $('.share').click(function(e) {
+    var metadataId = e.currentTarget.getAttribute('metadataid');
+
+    $.ajax({
+      type: "POST",
+      url: "https://api.parse.com/1/functions/shareImage",
+      headers: {
+        "X-Parse-Application-Id": appid,
+        "X-Parse-REST-API-Key": restAPIKey
+      },
+      data: {
+        metadataId: metadataId
+      }
+    }).done(function(msg) {
+      console.log("Data Saved");
+    });
+  });
+
+
+  $('.adsimage').click(function(event) {
+    $('.popup-image').attr("src", event.target.src);
+  });
+
+
+  $('.adsimage').click(function(event) {
+    var str = event.target.alt;
+    var parts = str.split(",");
+      $('.companyName').text(parts[0]);
+      $('.promoInfo').text(parts[1]);
+      $('.expiry').text(parts[2]);
+      $('.site').text(parts[3]);
+  });
+e
+  $('.approve').click(function(event) {
+    var metadataId = event.currentTarget.getAttribute('metadataid');
+    $.ajax({
+      type: "POST",
+      url: "https://the-central-market.parseapp.com/i/" + metadataId + "/approve",
+      headers: {
+        "X-Parse-Application-Id": appid,
+        "X-Parse-REST-API-Key": restAPIKey
+      },
+      data: {
+        metadataId: metadataId
+      }
+    }).done(function(msg) {
+      console.log("Image Approve");
+    });
+    //Do POST to /:id/approve
+  });
+
+  $('.reject').click(function(event) {
+    var metadataId = event.currentTarget.getAttribute('metadataid');
+    $.ajax({
+      type: "POST",
+      url: "https://the-central-market.parseapp.com/i/" + metadataId + "/reject",
+      headers: {
+        "X-Parse-Application-Id": appid,
+        "X-Parse-REST-API-Key": restAPIKey
+      },
+      data: {
+        metadataId: metadataId
+      }
+    }).done(function(msg) {
+      console.log("Image Reject");
+    });
+    //Do POST to /:id/reject
+  });
+
+  //Button in popup
+  $('#approve-button').click(function(event) {
+    //Do POST to /:id/approve
+  });
+
+  //Button in popup
+  $('#reject-button').click(function(event) {
+    //Do POST to /:id/reject
+  });
+
+});
 
 Uploader = Backbone.View.extend({
   events: {
@@ -57,145 +171,3 @@ Uploader = Backbone.View.extend({
     return false;
   }
 });
-
-
-
-$(function() {
-  // Make all of special links magically post the form
-  // when it has a particular data-action associated
-  $("a[data-action='post']").click(function(e) {
-    var el = $(e.target);
-    el.closest("form").submit();
-    return false;
-  });
-
-  $('.like').click(function(e) {
-    var metadataId = e.currentTarget.getAttribute('metadataid');
-
-    $.ajax({
-      type: "POST",
-      url: "https://api.parse.com/1/functions/likeImage",
-      headers: {
-        "X-Parse-Application-Id": "MZVlTYAzuOWwZ1JH9xMAhlVpyEG2banAtMVaCiI3",
-        "X-Parse-REST-API-Key": "J3uSlWUvKMyC31ibsJ7GVWKXjHArX7q1GVTuacfj"
-      },
-      data: {
-        metadataId: metadataId
-      }
-    }).done(function(msg) {
-      console.log("Data Saved");
-    });
-  });
-
-  $('.share').click(function(e) {
-    var metadataId = e.currentTarget.getAttribute('metadataid');
-
-    $.ajax({
-      type: "POST",
-      url: "https://api.parse.com/1/functions/shareImage",
-      headers: {
-        "X-Parse-Application-Id": "MZVlTYAzuOWwZ1JH9xMAhlVpyEG2banAtMVaCiI3",
-        "X-Parse-REST-API-Key": "J3uSlWUvKMyC31ibsJ7GVWKXjHArX7q1GVTuacfj"
-      },
-      data: {
-        metadataId: metadataId
-      }
-    }).done(function(msg) {
-      console.log("Data Saved");
-    });
-  });
-
-
-  $('.adsimage').click(function(event) {
-    $('#popup-image').attr("src", event.target.src);
-  });
-
-  $('.adsimage').click(function(event) {
-    var str = event.target.alt;
-    var parts = str.split(",");
-    if (parts[0] == "user"){
-      $('#companyName').text(parts[1]);
-      $('#promoInfo').text(parts[2]);
-      $('#expiry').text(parts[3]);
-      $('#site').text(parts[4]);
-    }else if (parts[0] == "merchant-current"){
-      $('#promoInfo').text(parts[1]);
-      $('#category').text(parts[2]);
-      $('#expiry').text(parts[3]);
-      $('#site').text(parts[4]);
-    };
-
-
-  });
-
-  $('.approve').click(function(event) {
-    var metadataId = event.currentTarget.getAttribute('metadataid');
-    $.ajax({
-      type: "POST",
-      url: "https://the-central-market.parseapp.com/i/" + metadataId + "/approve",
-      headers: {
-        "X-Parse-Application-Id": "MZVlTYAzuOWwZ1JH9xMAhlVpyEG2banAtMVaCiI3",
-        "X-Parse-REST-API-Key": "J3uSlWUvKMyC31ibsJ7GVWKXjHArX7q1GVTuacfj"
-      },
-      data: {
-        metadataId: metadataId
-      }
-    }).done(function(msg) {
-      console.log("Image Approve");
-    });
-    //Do POST to /:id/approve
-  });
-
-  $('.reject').click(function(event) {
-    var metadataId = event.currentTarget.getAttribute('metadataid');
-    $.ajax({
-      type: "POST",
-      url: "https://the-central-market.parseapp.com/i/" + metadataId + "/reject",
-      headers: {
-        "X-Parse-Application-Id": "MZVlTYAzuOWwZ1JH9xMAhlVpyEG2banAtMVaCiI3",
-        "X-Parse-REST-API-Key": "J3uSlWUvKMyC31ibsJ7GVWKXjHArX7q1GVTuacfj"
-      },
-      data: {
-        metadataId: metadataId
-      }
-    }).done(function(msg) {
-      console.log("Image Reject");
-    });
-    //Do POST to /:id/reject
-  });
-
-  //Button in popup
-  $('#approve-button').click(function(event) {
-    //Do POST to /:id/approve
-  });
-
-  //Button in popup
-  $('#reject-button').click(function(event) {
-    //Do POST to /:id/reject
-  });
-
-});
-
-
-function login() {
-  Parse.FacebookUtils.logIn("public_profile, email, user_friends", {
-    success: function(user) {
-      window.location.href = "https://the-central-market.parseapp.com/user";
-    },
-    error: function(user, error) {
-      console.log(JSON.stringify(error, null, " "));
-      alert("User cancelled the facebook login or did not fully authorize");
-    }
-  });
-};
-
-function logout() {
-  Parse.FacebookUtils.logOut();
-}
-
-
-function checkPermissions() {
-  FB.api('/me/permissions', function(response) {
-    console.log(response);
-  });
-}
